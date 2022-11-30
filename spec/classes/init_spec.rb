b84279f35d56cf71b522dce04c9dcc8a833f9cd6
Phil Friderici <phil.friderici@elits.com>
2022-11-30 18:04:15 +0000

    Stricter unit tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -83,6 +83,152 @@ describe 'auditd' do
           it { is_expected.to contain_file('auditd-auditd-plugin-auoms.conf') }
         end
 
+        context 'with buffer_size set to valid value' do
+          let(:params) { { buffer_size: 242 } }
+
+          it { is_expected.to contain_concat__fragment('auditd_rules_begin').with_content(%r{^-b 242$}) }
+        end
+
+        context 'with failure_mode set to valid value' do
+          let(:params) { { failure_mode: 2 } }
+
+          it { is_expected.to contain_concat__fragment('auditd_rules_begin').with_content(%r{^-f 2$}) }
+        end
+
+        context 'with immutable set to valid value' do
+          let(:params) { { immutable: true } }
+
+          it { is_expected.to contain_concat__fragment('auditd_rules_end').with_content(%r{^-e 2$}) }
+        end
+
+        context 'with dir set to valid value' do
+          let(:params) { { dir: '/test/ing' } }
+
+          it { is_expected.to contain_file('/test/ing') }
+        end
+
+        context 'with mode set to valid value' do
+          let(:params) { { mode: '0242' } }
+
+          it { is_expected.to contain_file('/etc/audit').with_mode('0242') }
+        end
+
+        context 'with owner set to valid string value' do
+          let(:params) { { owner: 'test' } }
+
+          it { is_expected.to contain_file('/etc/audit').with_owner('test') }
+        end
+
+        context 'with owner set to valid integer value' do
+          let(:params) { { owner: 242 } }
+
+          it { is_expected.to contain_file('/etc/audit').with_owner(242) }
+        end
+
+        context 'with group set to valid string value' do
+          let(:params) { { group: 'test' } }
+
+          it { is_expected.to contain_file('/etc/audit').with_group('test') }
+        end
+
+        context 'with group set to valid integer value' do
+          let(:params) { { group: 242 } }
+
+          it { is_expected.to contain_file('/etc/audit').with_group(242) }
+        end
+
+        context 'with config set to valid value' do
+          let(:params) { { 'config': { 'log_file' => '/test/ing' } } }
+
+          it { is_expected.to contain_file('/etc/audit/auditd.conf').with_content(%r{log_file = \/test\/ing}) }
+        end
+
+        context 'with config_path set to valid value' do
+          let(:params) { { config_path: '/test/ing' } }
+
+          it { is_expected.to contain_file('/test/ing') }
+        end
+
+        context 'with config_mode set to valid value' do
+          let(:params) { { config_mode: '0242' } }
+
+          it { is_expected.to contain_file('/etc/audit/auditd.conf').with_mode('0242') }
+        end
+
+        context 'with config_owner set to valid string value' do
+          let(:params) { { config_owner: 'test' } }
+
+          it { is_expected.to contain_file('/etc/audit/auditd.conf').with_owner('test') }
+        end
+
+        context 'with config_owner set to valid integer value' do
+          let(:params) { { config_owner: 242 } }
+
+          it { is_expected.to contain_file('/etc/audit/auditd.conf').with_owner(242) }
+        end
+
+        context 'with config_group set to valid string value' do
+          let(:params) { { config_group: 'test' } }
+
+          it { is_expected.to contain_file('/etc/audit/auditd.conf').with_group('test') }
+        end
+
+        context 'with config_group set to valid integer value' do
+          let(:params) { { config_group: 242 } }
+
+          it { is_expected.to contain_file('/etc/audit/auditd.conf').with_group(242) }
+        end
+
+        context 'with package_name set to valid value' do
+          let(:params) { { package_name: 'testing' } }
+
+          it { is_expected.to contain_package('testing') }
+        end
+
+        context 'with package_ensure set to valid value' do
+          let(:params) { { package_ensure: '2.4.2' } }
+
+          os_package = if facts[:os]['family'] == 'RedHat'
+                         'audit'
+                       else
+                         'auditd'
+                       end
+
+          it { is_expected.to contain_package(os_package).with_ensure('2.4.2') }
+        end
+
+        context 'with package_manage set to valid value' do
+          let(:params) { { package_manage: false } }
+
+          it { is_expected.not_to contain_package('auditd') }
+        end
+
+        context 'with service_enable set to valid value' do
+          let(:params) { { service_enable: false } }
+
+          it { is_expected.to contain_service('auditd').with_enable(false) }
+        end
+
+        context 'with service_ensure set to valid value' do
+          let(:params) { { service_ensure: 'stopped' } }
+
+          it { is_expected.to contain_service('auditd').with_ensure('stopped') }
+        end
+
+        # TODO: This functionality is not working and needs fixing
+        # If the service is not managed these resources will fail
+        # auditd::config - concat { $auditd::rules_file }
+        # auditd::config - file { $auditd::config_path }
+        # auditd::plugin - file { "auditd-${plugin_type}-plugin-${name}.conf"}
+        # because the notify parameter can not find the service['auditd'] anymore.
+        #
+        # context 'with service_manage set to valid value' do
+        #   let(:params) { { service_manage: false } }
+        #
+        #   it { is_expected.not_to contain_service('audit') }  # RedHat
+        #   it { is_expected.not_to contain_service('auditd') } # others
+        # end
+
         context 'with service_override set to valid value' do
           let(:params) { { service_override: 'testing' } }
 
@@ -94,7 +240,6 @@ describe 'auditd' do
               'mode'   => '0750',
             )
           }
-
           it {
             is_expected.to contain_file('/etc/systemd/system/auditd.service.d/override.conf').only_with(
               'ensure'  => 'file',
@@ -104,6 +249,164 @@ describe 'auditd' do
               'content' => 'testing',
             )
           }
+        end
+
+        context 'with plugin_dir set to valid value' do
+          let(:params) { { plugin_dir: '/test/ing' } }
+
+          it { is_expected.to contain_file('/test/ing') }
+        end
+
+        context 'with plugin_dir_mode set to valid value' do
+          let(:params) { { plugin_dir_mode: '0242' } }
+
+          it { is_expected.to contain_file('/etc/audit/plugins.d').with_mode('0242') }
+        end
+
+        context 'with plugin_dir_owner set to valid string value' do
+          let(:params) { { plugin_dir_owner: 'test' } }
+
+          it { is_expected.to contain_file('/etc/audit/plugins.d').with_owner('test') }
+        end
+
+        context 'with plugin_dir_owner set to valid integer value' do
+          let(:params) { { plugin_dir_owner: 242 } }
+
+          it { is_expected.to contain_file('/etc/audit/plugins.d').with_owner(242) }
+        end
+
+        context 'with plugin_dir_group set to valid string value' do
+          let(:params) { { plugin_dir_group: 'test' } }
+
+          it { is_expected.to contain_file('/etc/audit/plugins.d').with_group('test') }
+        end
+
+        context 'with plugin_dir_group set to valid integer value' do
+          let(:params) { { plugin_dir_group: 242 } }
+
+          it { is_expected.to contain_file('/etc/audit/plugins.d').with_group(242) }
+        end
+
+        context 'with plugins set to valid value' do
+          let(:params) do
+            {
+              plugins: {
+                test1: {
+                  active: 'no',
+                  direction: 'out',
+                  path: '/test/ing1',
+                },
+                test2: {
+                  active: 'yes',
+                  direction: 'out',
+                  path: '/test/ing2',
+                },
+              },
+            }
+          end
+
+          it {
+            is_expected.to contain_auditd__plugin('test1').with(
+              'active'    => 'no',
+              'direction' => 'out',
+              'path'      => '/test/ing1',
+            )
+          }
+
+          it {
+            is_expected.to contain_auditd__plugin('test2').with(
+              'active'    => 'yes',
+              'direction' => 'out',
+              'path'      => '/test/ing2',
+            )
+          }
+
+          it { is_expected.to contain_file('auditd-auditd-plugin-test1.conf') } # only needed for 100% resource coverage
+          it { is_expected.to contain_file('auditd-auditd-plugin-test2.conf') } # only needed for 100% resource coverage
+        end
+
+        context 'with rules_dir set to valid value' do
+          let(:params) { { rules_dir: '/test/ing' } }
+
+          it { is_expected.to contain_file('/test/ing') }
+        end
+
+        context 'with rules_dir_mode set to valid value' do
+          let(:params) { { rules_dir_mode: '0242' } }
+
+          it { is_expected.to contain_file('/etc/audit/rules.d').with_mode('0242') }
+        end
+
+        context 'with rules_dir_owner set to valid string value' do
+          let(:params) { { rules_dir_owner: 'test' } }
+
+          it { is_expected.to contain_file('/etc/audit/rules.d').with_owner('test') }
+        end
+
+        context 'with rules_dir_owner set to valid integer value' do
+          let(:params) { { rules_dir_owner: 242 } }
+
+          it { is_expected.to contain_file('/etc/audit/rules.d').with_owner(242) }
+        end
+
+        context 'with rules_dir_group set to valid string value' do
+          let(:params) { { rules_dir_group: 'test' } }
+
+          it { is_expected.to contain_file('/etc/audit/rules.d').with_group('test') }
+        end
+
+        context 'with rules_dir_group set to valid integer value' do
+          let(:params) { { rules_dir_group: 242 } }
+
+          it { is_expected.to contain_file('/etc/audit/rules.d').with_group(242) }
+        end
+
+        context 'with rules_file set to valid value' do
+          let(:params) { { rules_file: '/test/ing' } }
+
+          it { is_expected.to contain_concat('/test/ing') }
+          it { is_expected.to contain_concat__fragment('auditd_rules_begin').with_target('/test/ing') }
+          it { is_expected.to contain_concat__fragment('auditd_rules_end').with_target('/test/ing') }
+        end
+
+        context 'with rules_file set to valid value when rules is also set' do
+          let(:params) { { rules_file: '/test/ing', rules: { test5: {} } } }
+
+          it { is_expected.to contain_concat__fragment('auditd_fragment_test5').with_target('/test/ing') }
+          it { is_expected.to contain_auditd__rule('test5') } # only needed for 100% resource coverage
+        end
+
+        context 'with rules set to valid value' do
+          let(:params) do
+            {
+              rules: {
+                test3: {
+                  content: 'testing3',
+                },
+                test4: {
+                  content: 'testing4',
+                  order:   50,
+                },
+              },
+            }
+          end
+
+          it {
+            is_expected.to contain_auditd__rule('test3').only_with(
+              'content' => 'testing3',
+              'order'   => 10,
+            )
+          }
+
+          it {
+            is_expected.to contain_auditd__rule('test4').only_with(
+              'content' => 'testing4',
+              'order'   => 50,
+            )
+          }
+
+          it { is_expected.to contain_concat__fragment('auditd_fragment_test3') } # only needed for 100% resource coverage
+          it { is_expected.to contain_concat__fragment('auditd_fragment_test4') } # only needed for 100% resource coverage
         end
       end
     end
--- spec/classes/rule_spec.rb
+++ spec/classes/rule_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
-# rubocop:disable Layout/FirstHashElementIndentation
 describe 'auditd::rule', type: :define do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
@@ -8,30 +7,49 @@ describe 'auditd::rule', type: :define do
         let(:facts) { facts }
         let(:pre_condition) { 'include ::auditd' }
 
-        context 'empty content' do
+        context 'with default values for parameters' do
           let(:title) { '-w /var/run/utmp -p wa -k session' }
 
+          it { is_expected.to compile }
           it {
-            is_expected.to compile
-            is_expected.to create_concat__fragment('auditd_fragment_-w /var/run/utmp -p wa -k session').with({
-              content: "-w /var/run/utmp -p wa -k session\n\n"
-            })
+            is_expected.to create_concat__fragment('auditd_fragment_-w /var/run/utmp -p wa -k session').only_with(
+              'target'  => '/etc/audit/rules.d/audit.rules',
+              'order'   => 10,
+              'content' => "-w /var/run/utmp -p wa -k session\n\n",
+            )
           }
         end
 
+        context 'with content set to empty string' do
+          let(:title) { 'testing' }
+          let(:params) { { content: '' } }
+
+          it { is_expected.to create_concat__fragment('auditd_fragment_testing').with_content(%r{testing\n\n}) }
+        end
+
+        context 'with content set to valid value' do
+          let(:title) { 'testing' }
+          let(:params) { { content: 'test' } }
+
+          it { is_expected.to create_concat__fragment('auditd_fragment_testing').with_content(%r{# testing\ntest\n\n}) }
+        end
+
+        context 'with order set to valid value' do
+          let(:title) { 'testing' }
+          let(:params) { { order: 3 } }
+
+          it { is_expected.to create_concat__fragment('auditd_fragment_testing').with_order(3) }
+        end
+
+        # real life examples
         context 'insmod rule' do
           let(:title) { 'insmod' }
-          let(:params) do
-            {
-              'content' => '-w /sbin/insmod -p x -k modules',
-            }
-          end
+          let(:params) { { 'content' => '-w /sbin/insmod -p x -k modules' } }
 
           it {
-            is_expected.to compile
-            is_expected.to create_concat__fragment('auditd_fragment_insmod').with({
-              content: "# insmod\n-w /sbin/insmod -p x -k modules\n\n"
-            })
+            is_expected.to create_concat__fragment('auditd_fragment_insmod').with(
+              'content' => "# insmod\n-w /sbin/insmod -p x -k modules\n\n",
+            )
           }
         end
 
@@ -45,10 +63,11 @@ describe 'auditd::rule', type: :define do
           end
 
           it {
-            is_expected.to compile
-            is_expected.to create_concat__fragment('auditd_fragment_sudoers_changes').with({
-              content: "# sudoers_changes\n-w /etc/sudoers -p wa -k scope\n\n"
-            })
+            is_expected.to create_concat__fragment('auditd_fragment_sudoers_changes').only_with(
+              'target'  => '/etc/audit/rules.d/audit.rules',
+              'order'   => 50,
+              'content' => "# sudoers_changes\n-w /etc/sudoers -p wa -k scope\n\n",
+            )
           }
         end
       end
